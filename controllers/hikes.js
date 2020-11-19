7c4a3e9cdf4c3139b64eb8a040ee10cee2d0dc70
kleddy17 <kleddy17@gmail.com>
2020-11-18 20:05:43 -0500

    configuration for heroku

--- controllers/hikes.js
+++ controllers/hikes.js
@@ -8,7 +8,7 @@ const isLoggedIn = require('../middleware/isLoggedIn')
 
 
 router.get('/', isLoggedIn, (req,res)=>{
-  axios.get(`https://www.hikingproject.com/data/get-trails?lat=44.2706207&lon=-71.3042419&maxDistance=50&maxResults=100&key=200966308-be5dfe33bfed32e2f9e2bb337446b45a`)
+  axios.get(`https://www.hikingproject.com/data/get-trails?lat=44.2706207&lon=-71.3042419&maxDistance=50&maxResults=100&key=${process.env.API_KEY}`)
     .then(response => {
         // console.log("hiii", response.data)
         const trail = response.data

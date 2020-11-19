61520f2cd0e3e89fcf31e66b822e603540060225
kleddy17 <kleddy17@gmail.com>
2020-11-18 21:33:09 -0500

    the S

--- migrations/20201109160306-create-user-hike.js
+++ migrations/20201109160306-create-user-hike.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('userHike', {
+    await queryInterface.createTable('userHikes', {
       id: {
         allowNull: false,
         autoIncrement: true,

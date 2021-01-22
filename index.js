e412d635fa13e72e78f38201617b1781d7281334
kleddy17 <kleddy17@gmail.com>
2021-01-22 10:47:16 -0500

    changed app.use

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
-app.use(express.static(__dirname + '/public'))
+// app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'))
 
 //session middleware
 app.use(session({

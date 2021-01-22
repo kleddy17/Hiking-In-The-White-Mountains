93bba1b5e923d466bba2ec963c69f1131dd78f92
kleddy17 <kleddy17@gmail.com>
2021-01-22 09:59:52 -0500

    npm i bootstrap

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
-app.use(express.static(__dirname + '/public/css'))
+app.use(express.static(__dirname + '/public'))
 
 //session middleware
 app.use(session({
